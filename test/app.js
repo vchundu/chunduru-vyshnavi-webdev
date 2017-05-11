8c77188d726dc3330fbe46c15fd50d715027409f
Vyshnavi Chunduru <chunduru.v@husky.neu.edu>
2017-05-11 07:38:02 -0400

    fixed where connection occurred

--- test/app.js
+++ test/app.js
@@ -4,15 +4,16 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
-
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+    // checks connection
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += '@ds137101.mlab.com:37101/heroku_ppjl6vwp'; // user yours
     }
+
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
